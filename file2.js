c6c193dbee7fd10989d8be382712d7dcab074839
andrew111123214 <138983207+andrew111123214@users.noreply.github.com>
2024-11-12 06:28:58 +0300

    initial commit

--- file2.js
+++ file2.js
@@ -3,15 +3,18 @@ let button1 = document.querySelector('.button1');
 let input2 = document.querySelector('.input2');
 let button2 = document.querySelector('.button2');
 let button3 = document.querySelector('.button3');
+button1.addEventListener('click', inputValue);
+button2.addEventListener('click', inputValue1);
+button3.addEventListener('click', square);
 function inputValue(input1)
 {
-    console.log();
+    console.log(input1.value);
 }
 function inputValue1(input2)
 {
-    console.log();
+    console.log(input2.value);
 }
 function square(input1,input2)
 {
-    console.log(input1*input2*0,5);
+    console.log(input1*input2*0.5);
 }
