17947225bff24af4ed1eac72a34ac9d87a83c5a3
andrew111123214 <138983207+andrew111123214@users.noreply.github.com>
2024-11-12 05:35:19 +0300

    initial commit

--- file2.js
+++ file2.js
@@ -1,8 +1,9 @@
 let input1 = document.querySelector('.input1');
 let button1 = document.querySelector('.button1');
-button1.onclick = function(input1,input2)
+button3.onclick = function()
 {
-    return 0,5*input1*input2
+    console.log(input1*input2);
+    
 }
 let input2 = document.querySelector('.input2');
 let button2 = document.querySelector('.button2');
