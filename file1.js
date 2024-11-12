ae597efd8eea61aa2462e1ed2f7786a3be20c524
andrew111123214 <138983207+andrew111123214@users.noreply.github.com>
2024-11-12 04:09:52 +0300

    initial commit

---
+++ file1.js
@@ -0,0 +1,29 @@
+var s= prompt("Введите номер дня недели","")*1
+switch (s) {
+    case 1:alert("")
+        
+        break;
+    case 1:alert("Понедельник")
+        
+        break;
+    case 2:alert("Виорник")
+        
+        break;
+    case 3:alert("Среда")
+        
+        break;
+    case 4:alert("Четверг")
+        
+        break;
+    case 5:alert("Пятница")
+    
+        break;
+    case 6:alert("Суббота")
+        
+        break;
+    case 7:alert("Воскресенье")
+        
+        break;
+    default:alert("Ошиббка в номере дня")
+        break;
+}
