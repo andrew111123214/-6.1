91bd7022bc2ba31e57b27bde404f1446103424c7
andrew111123214 <138983207+andrew111123214@users.noreply.github.com>
2024-11-12 19:20:41 +0300

    initial commit

--- file2.js
+++ file2.js
@@ -6,14 +6,16 @@ const buttonThree = document.querySelector('.buttonThree');
 function enterOne()
 {
     console.log(inputOne.value);
+    inputOne.value = ''
 }
 function enterTwo()
 {
     console.log(inputTwo.value);
+    inputTwo.value = '';
 }
 function result()
 {
-    console.log(inputOne.value*inputTwo.value*0.5);
+    alert(inputOne.value*inputTwo.value*0.5);
 }
 buttonOne.addEventListener('click', enterOne);
 buttonTwo.addEventListener('click', enterTwo);
