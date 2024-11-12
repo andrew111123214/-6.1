3a92a6c24440cc9c694a9ee9cd0deb4ca1fd173f
andrew111123214 <138983207+andrew111123214@users.noreply.github.com>
2024-11-12 23:10:19 +0300

    initial commit

--- file3.js
+++ file3.js
@@ -1,3 +1,16 @@
 let input = document.querySelector('input');
 const button = document.querySelector('button');
-button.addEventListener = func();
+button.addEventListener = createTable();
+let elem = document.querySelector('#elem');
+function createTable(elements,cols,rows)
+{
+    let table =  document.createElement('table');
+    for (let i = 0; i < array.length; i++) 
+        {
+        const element = array[i];
+        for (let j = 0; j < array.length; j++) 
+        {
+            const element = array[j];
+        }
+    }
+}
