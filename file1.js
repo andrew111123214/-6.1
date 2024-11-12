34a0a8280dba3e9c0de10504840575b6fe797e1f
andrew111123214 <138983207+andrew111123214@users.noreply.github.com>
2024-11-12 20:47:31 +0300

    initial commit

--- file1.js
+++ file1.js
@@ -1,33 +1,33 @@
 let input = document.querySelector('input');
 let button =  document.querySelector('button');
-switch (input.value) 
-{
-    case 1:alert("Понедельник")
-        
-        break;
-    case 2:alert("Вторник")
-        
-        break;
-    case 3:alert("Среда")
-        
-        break;
-    case 4:alert("Четверг")
-        
-        break;
-    case 5:alert("Пятница")
-    
-        break;
-    case 6:alert("Суббота")
-        
-        break;
-    case 7:alert("Воскресенье")
-        
-        break;
-    default:alert("Ошиббка в номере дня")
-        break;
-}
 function enter()
 {
-    console.log();
+    switch (input.value) 
+    {
+        case 1:alert("Понедельник")
+        
+            break;
+        case 2:alert("Вторник")
+        
+            break;
+        case 3:alert("Среда")
+        
+            break;
+        case 4:alert("Четверг")
+        
+            break;
+        case 5:alert("Пятница")
+    
+            break;
+        case 6:alert("Суббота")
+        
+            break;
+        case 7:alert("Воскресенье")
+        
+            break;
+        default:alert("Ошиббка в номере дня")
+            break;
+    }
+    
 }
 button.addEventListener('click',enter);
