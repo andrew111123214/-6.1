87828b3dcb6d15ba7fca8dfc8855281e293be728
andrew111123214 <138983207+andrew111123214@users.noreply.github.com>
2024-11-12 23:30:01 +0300

    initial commit

--- file3.js
+++ file3.js
@@ -1,11 +1,16 @@
 var input = document.querySelector('input');
 const button = document.querySelector('button');
-var input = document.querySelector('inputIn');
+var input = document.querySelector('inputRows');
 const button1 = document.querySelector('.btn1');
-var input = document.querySelector('.inputIn1');
+var input = document.querySelector('.inputCols');
 const button2 = document.querySelector('.btn2');
+const button3 = document.querySelector('.createTable');
 button.addEventListener = createTable();
 var elem = document.querySelector('#elem');
+function btn1()
+{
+
+}
 createTable(elem,cols,rows);
 function createTable(elements,cols,rows)
 {
