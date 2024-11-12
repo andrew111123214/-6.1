bcbcbb8117edb1dcf2b82d5ccb5c27edbec6a561
andrew111123214 <138983207+andrew111123214@users.noreply.github.com>
2024-11-12 18:30:55 +0300

    initial commit

--- file2.js
+++ file2.js
@@ -1,10 +1,20 @@
-let input1 = document.querySelector('.input1');
-let button1 = document.querySelector('.button1');
-let input2 = document.querySelector('.input2');
-let button2 = document.querySelector('.button2');
-let button3 = document.querySelector('.button3');
-button1.addEventListener('click', enter);
-function enter()
+let inputOne = document.querySelector('.input1');
+let buttonOne = document.querySelector('.button1');
+let inputTwo = document.querySelector('.input2');
+let buttonTwo = document.querySelector('.button2');
+let buttonThree = document.querySelector('.button3');
+buttonOne.addEventListener('click', enteOne);
+function enterOne()
+{
+    console.log('click');
+}
+buttonTwo.addEventListener('click', enterTwo);
+function enterTwo()
+{
+    console.log('click');
+}
+buttonThree.addEventListener('click', result);
+function result()
 {
     console.log('click');
 }
