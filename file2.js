356abae5d2b1ccb0683f0725b4aa1867c9bba62d
andrew111123214 <138983207+andrew111123214@users.noreply.github.com>
2024-11-12 05:03:54 +0300

    initial commit

--- file2.js
+++ file2.js
@@ -1,6 +1,3 @@
-var a = prompt("Введите значение основание треугольника:","")*1
-var h = promt("Введите значение высоты треугольника:","")*1
-function square(a,h)
-{
-    return 0.5*a*h
-}
+let a=2;
+let b = 4;
+console.log(a);
