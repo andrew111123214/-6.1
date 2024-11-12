83345e8245f0b8b1faf7cc95cd5925a7c0cbf806
andrew111123214 <138983207+andrew111123214@users.noreply.github.com>
2024-11-12 05:18:13 +0300

    initial commit

--- file2.js
+++ file2.js
@@ -1,6 +1,9 @@
-let a=2;
-let b = 4;
 let input1 = document.querySelector('.input1');
 let button1 = document.querySelector('.button1');
 let input2 = document.querySelector('.input2');
 let button2 = document.querySelector('.button2');
+function fun(input1,input2)
+{
+    return 0,5*input1*input2
+}
+console.log(fun);
