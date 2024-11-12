a33522576a43b0c538a4ef623a56c266f437ccfb
andrew111123214 <138983207+andrew111123214@users.noreply.github.com>
2024-11-12 19:32:04 +0300

    initial commit

--- file2.js
+++ file2.js
@@ -18,7 +18,6 @@ function enterTwo()
 function result()
 {
     console.log(a*h*0.5);
-    alert("Площадь треугольника",a*b*0.5);
 }
 buttonOne.addEventListener('click', enterOne);
 buttonTwo.addEventListener('click', enterTwo);
