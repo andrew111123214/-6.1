bd02e13178646605b5b537d5eb9404a230b54298
andrew111123214 <138983207+andrew111123214@users.noreply.github.com>
2024-11-15 02:22:09 +0300

    initial commit

--- file4.js
+++ file4.js
@@ -1,10 +1,10 @@
 var inputOne = document.querySelector('.inputOne');
 var inputTwo = document.querySelector('.inputTwo');
-const button = document.querySelector('button');
+const button = document.getElementById('btn');
 function square()
 {
-    let valueOne = parseInt(inputOne.value);
-    let valueTwo = parseInt(inputTwo.value);
+    let valueOne = parseFloat(inputOne.value);
+    let valueTwo = parseFloat(inputTwo.value);
     console.log(valueTwo*valueOne*0.5);
 }
 button.addEventListener('click',square);
