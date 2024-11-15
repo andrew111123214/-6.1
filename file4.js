29a4040ce54755772fa24608fe20577edc3ffadb
andrew111123214 <138983207+andrew111123214@users.noreply.github.com>
2024-11-15 22:22:50 +0300

    initial commit

--- file4.js
+++ file4.js
@@ -1,5 +1,16 @@
 const formElement = document.forms.main;
-const formElementInputOne = formElement.InputOne
+const formElementInputOne = formElement.inputOne
 const formElementInputTwo = formElement.inputTwo;
-const formElementButton = formElement.btn;
+const button = document.querySelector('button[name="btn"]');
+function square()
+{
+    valueOne = parseFloat(formElementInputOne.value);
+    valueTwo = parseFloat(formElementInputTwo.value);
+    let sqare = valueOne*valueTwo*0.5
+    console.log(sqare);
+    alert("Площадь треугольника = "+ (sqare));
+    formElementInputOne.value = "";
+    formElementInputTwo.value = "";
+}
 console.log(formElement.elements);
+button.addEventListener('click',square);
