7d9b11013f6df6dbac6bff49d3c1e8960ac2edc7
andrew111123214 <138983207+andrew111123214@users.noreply.github.com>
2024-11-28 06:09:47 +0300

    initial commit

--- file7.js
+++ file7.js
@@ -1,34 +1,26 @@
 const formElement = document.forms.main;
-const formElementRadio_1 = formElement.radio_1;
-const formElementRadio_2 = formElement.radio_2;
-const formElementRadio_3 = formElement.radio_3;
-const formElementRadio_4 = formElement.radio_4;
+const formElementRadio = formElement.elements['radio'];
 const formElementReset = formElement.reset;
 const button = document.querySelector('button[name="btn"]');
 console.log(formElement.elements);
-function fun()
-{
-    var k;
-    if (a.formElementRadio_1.checked) 
-        {
-            k = formElementRadio_1.value;
-        } 
-    if(a.formElementRadio_2.checked)
-        {
-            k = formElementRadio_2.value;
-        }
-    if(a.formElementRadio_3.checked)
-        {
-            k = formElementRadio_3.value;
-        }
-    if(a.formElementRadio_4.checked)
-        {
-            k = formElementRadio_4.value;
-        }
-        alert("Форма витража - "+ fun());
-}
 button.addEventListener('click',function()
 {
-    fun();
-}
+    let k;
+    for (let i=0; i < formElementRadio.length; i++) 
+        {
+            if(formElementRadio[i].checked)
+                {
+                k = formElementRadio[i].value;
+                }
+        }
+        if(k)
+            {
+                alert("Выбрана форма:" +k);
+
+            }
+        else
+        {
+            alert("Выберете хотя бы одно значение!");
+        }
+    }
 );
