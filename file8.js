9d927512b8099a77351fa548757b2422ba479416
andrew111123214 <138983207+andrew111123214@users.noreply.github.com>
2024-11-30 22:41:27 +0300

    initial commit

--- file8.js
+++ file8.js
@@ -2,24 +2,29 @@ const formElement = document.forms.main;
 const formElementRadio = formElement.elements['radio'];
 const formElementRadio_1 = formElement.elements['radio_1'];
 const button = document.querySelector('button[name="btn"]');
+const image = document.querySelector('img');
 console.log(formElement.elements);
 function activity()
 {
-    let k;
-    let m;
+
     for (let i = 0; i < formElementRadio.length; i++) 
         {
             if (formElementRadio[i].checked) 
                 {
-                k= formElementRadio[i].value;
-            }
-            for (let i = 0; i<formElementRadio_1.length; i++) 
-                {
-                if(formElementRadio_1[i].cheked)
-                {
-                    m = formElementRadio_1[i].valign;
+                image.style.align=formElementRadio[i].value.toLowerCase();
                 }
         }
-    }
+    for (let j = 0; j<formElementRadio_1.length; j++) 
+        {
+            if(formElementRadio_1[j].cheked)
+                {
+                    image.style.verticalAlign=formElementRadio_1[j].value.toLowerCase();
+                }
 
+        }
 }
+button.addEventListener('click',function()
+{
+    activity();
+}
+);
