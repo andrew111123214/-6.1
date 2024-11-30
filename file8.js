16994f50e7193a58fc3ebe6447e0cccb6a9fa8cb
andrew111123214 <138983207+andrew111123214@users.noreply.github.com>
2024-11-30 20:37:32 +0300

    initial commit

--- file8.js
+++ file8.js
@@ -3,3 +3,23 @@ const formElementRadio = formElement.elements['radio'];
 const formElementRadio_1 = formElement.elements['radio_1'];
 const button = document.querySelector('button[name="btn"]');
 console.log(formElement.elements);
+function activity()
+{
+    let k;
+    let m;
+    for (let i = 0; i < formElementRadio.length; i++) 
+        {
+            if (formElementRadio[i].checked) 
+                {
+                k= formElementRadio[i].value;
+            }
+            for (let i = 0; i<formElementRadio_1.length; i++) 
+                {
+                if(formElementRadio_1[i].cheked)
+                {
+                    m = formElementRadio_1[i].valign;
+                }
+        }
+    }
+
+}
