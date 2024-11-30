1444014b8fea7832b4fe07f11a3ec7a1a741ec2f
andrew111123214 <138983207+andrew111123214@users.noreply.github.com>
2024-11-30 19:47:56 +0300

    initial commit

---
+++ file8.js
@@ -0,0 +1,5 @@
+const formElement = document.forms.main;
+const formElementRadio = formElement.elements['radio'];
+const formElementRadio_1 = formElement.elements['radio_1'];
+const button = document.querySelector('button[name="btn"]');
+console.log(formElement.elements);
