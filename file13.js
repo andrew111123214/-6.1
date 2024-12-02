5bf582836c7b3c415419ccd9920ae8d947870b85
andrew111123214 <138983207+andrew111123214@users.noreply.github.com>
2024-12-03 02:44:32 +0300

    initial commit

--- file13.js
+++ file13.js
@@ -1,26 +1,39 @@
 const formElement = document.forms.main;
-const formElementCheckBox = formElement.elements['check'];
+const formElementCheckBox1 = document.getElementById('y1');
+const formElementCheckBox2 = document.getElementById('y2');
+const formElementCheckBox3 = document.getElementById('y3');
+const formElementCheckBox4 = document.getElementById('y4');
+const formElementCheckBox5 = document.getElementById('y5');
+const formElementCheckBox6 = document.getElementById('y6');
 const button = document.querySelector('button[name="btn"]');
 console.log(formElement.elements);
 function fun()
 {
     let k=0;
-    let counter = 0;
-    for (let i=0; i < formElementCheckBox.length; i++) 
+    if (formElementCheckBox1.checked) 
         {
-            if(formElementCheckBox[i].checked)
-                {
-                    k += Number(formElementCheckBox[i].value);
-                    counter++;
-                }  
+            k+=Number(formElementCheckBox1.value);
         }
-        if (counter===0) 
+        if (formElementCheckBox2.checked) 
             {
-                alert("Вы настолько бездарны, что не знаете ни одного языка(");
-        }
-        else
-        {
-        alert("Ваша зарплата составит:" + k + "y.e.");
-        }
+                k+=Number(formElementCheckBox2.value);
+            }
+            if (formElementCheckBox3.checked) 
+                {
+                    k+=Number(formElementCheckBox3.value);
+                }
+                if (formElementCheckBox4.checked) 
+                    {
+                        k+=Number(formElementCheckBox4.value);
+                    }
+                    if (formElementCheckBox5.checked) 
+                        {
+                            k+=Number(formElementCheckBox5.value);
+                        }
+                        if (formElementCheckBox6.checked) 
+                            {
+                                k+=Number(formElementCheckBox6.value);
+                            }
+                            alert("Ваша зарплата:" + k);
 }
 button.addEventListener("click",fun);
