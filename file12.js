a4433aef0a0640ff3a61cf8ac994998a5e99d519
andrew111123214 <138983207+andrew111123214@users.noreply.github.com>
2024-12-03 02:16:03 +0300

    initial commit

--- file12.js
+++ file12.js
@@ -5,16 +5,22 @@ console.log(formElement.elements);
 function fun()
 {
     let k=0;
+    let counter = 0;
     for (let i=0; i < formElementCheckBox.length; i++) 
         {
             if(formElementCheckBox[i].checked)
                 {
                     k += Number(formElementCheckBox[i].value);
-                }    
+                    counter++;
+                }  
         }
-        alert("Ваша зарплата составит:" + k + "y.e.")
+        if (counter===0) 
+            {
+                alert("Вы настолько бездарны, что не знаете ни одного языка(");
+        }
+        else
+        {
+        alert("Ваша зарплата составит:" + k + "y.e.");
+        }
 }
-for (let i = 0; i < formElementCheckBox.length; i++) 
-    {
-    button.addEventListener("click",fun);
-}
+button.addEventListener("click",fun);
