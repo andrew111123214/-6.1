11ca5e7a20cb74c3ca0a68d630fdfdcb48d96aa6
andrew111123214 <andryushenka-belyaev-2000@mail.ru>
2024-12-11 03:39:29 +0300

    initial commit

--- file8.js
+++ file8.js
@@ -2,29 +2,20 @@ const formElement = document.forms.main;
 const formElementRadio = formElement.elements['radio'];
 const formElementRadio_1 = formElement.elements['radio_1'];
 const button = document.querySelector('button[name="btn"]');
-const image = document.querySelector('img');
-console.log(formElement.elements);
-function activity()
-{
-
-    for (let i = 0; i < formElementRadio.length; i++) 
-        {
-            if (formElementRadio[i].checked) 
-                {
-                image.style.align=formElementRadio[i].value.toLowerCase();
+const image = document.getElementById('image');
+    function activity() {
+            for (let i = 0; i < formElementRadio.length; i++) {
+                if (formElementRadio[i].checked) {
+                    image.style.textAlign = formElementRadio[i].value.toLowerCase();
+                    image.style.margin = formElementRadio[i].value.toLowerCase() === 'left' ? '0' : '0 auto';
                 }
-        }
-    for (let j = 0; j<formElementRadio_1.length; j++) 
-        {
-            if(formElementRadio_1[j].cheсked)
-                {
-                    image.style.verticalAlign=formElementRadio_1[j].value.toLowerCase();
+            }
+            for (let j = 0; j < formElementRadio_1.length; j++) {
+                if (formElementRadio_1[j].checked) {
+                    image.style.verticalAlign = formElementRadio_1[j].value.toLowerCase();
                 }
-
+            }
         }
-}
-button.addEventListener('click',function()
-{
-    activity();
-}
-);
+button.addEventListener('click', function() {
+activity();
+});
